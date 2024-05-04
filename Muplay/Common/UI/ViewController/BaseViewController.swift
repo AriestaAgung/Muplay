cdad6551b254000b00021eeaacdc5921f072b62c
Ariesta <ariesta.work@gmail.com>
2024-05-04 07:08:09 +0700

    Add change bg color ability to viewcontrollers and resizable content for text

--- Muplay/Common/UI/ViewController/BaseViewController.swift
+++ Muplay/Common/UI/ViewController/BaseViewController.swift
@@ -8,6 +8,8 @@
 import Cocoa
 import SnapKit
 class BaseViewController: NSViewController {
+    
+    private var bgColor: CGColor? 
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -16,8 +18,13 @@ class BaseViewController: NSViewController {
     
     private func setupView() {
         view.wantsLayer = true
-        view.layer?.backgroundColor = NSColor.white.withAlphaComponent(0.2).cgColor
+//        view.layer?.backgroundColor = bgColor
         view.layer?.cornerRadius = 20
+    }
+    
+    func setupBackgroundColor(color: CGColor = NSColor.white.withAlphaComponent(0.2).cgColor) {
+        bgColor = color
+        view.layer?.backgroundColor = bgColor
     }
     
 }
--- Muplay/Modules/Home/Views/HomeView.swift
+++ Muplay/Modules/Home/Views/HomeView.swift
@@ -14,13 +14,38 @@ class HomeView: BaseViewController, HomeViewDelegate {
     override func viewDidLoad() {
         super.viewDidLoad()
         presenter?.viewDidLoad()
+        setupBackgroundColor(color: .black)
+    }
+    
+    
+    func setupView() {
+//        bgColor = .clear
+        titleLabel.delegate = self
     }
     
     func setupTrackInfo(track: MediaRemoteTrackInfo) {
         self.titleLabel.stringValue = track.name ?? .emptyString
+        self.titleLabel.font = .boldSystemFont(ofSize: 20)
     }
     
 }
 
+extension HomeView: NSTextFieldDelegate {
+    func controlTextDidChange(_ obj: Notification) {
+        guard let field = obj.object as? NSTextField else {
+            return
+          }
 
+          // re-apply the string value, this is necessary for the text field to correctly calculate it's width
+          field.stringValue = field.stringValue
 
+          // calculate the new dimensions
+          let maxWidth: CGFloat = 500
+          let maxHeight: CGFloat = 50
+          let size = titleLabel.sizeThatFits(NSSize(width: maxWidth, height: maxHeight))
+
+          // apply them
+          field.frame = NSRect(x: field.frame.origin.x, y: field.frame.origin.y, width: size.width, height: size.height)
+        
+    }
+}
