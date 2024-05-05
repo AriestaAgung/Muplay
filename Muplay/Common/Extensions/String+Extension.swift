aaa14b24c9727fde0111ec97707799057361b7f2
Ariesta <ariesta.work@gmail.com>
2024-05-05 20:39:14 +0700

    Adding lyrics extractor

--- Muplay/Common/Extensions/String+Extension.swift
+++ Muplay/Common/Extensions/String+Extension.swift
@@ -9,4 +9,14 @@ import Foundation
 
 extension String {
     static let emptyString = ""
+    var htmlDecoded: String {
+        let decodedString = self.replacingOccurrences(of: "&quot;", with: "\"")
+            .replacingOccurrences(of: "&amp;", with: "&")
+            .replacingOccurrences(of: "&lt;", with: "<")
+            .replacingOccurrences(of: "&gt;", with: ">")
+            .replacingOccurrences(of: "&nbsp;", with: " ")
+            .replacingOccurrences(of: "&apos;", with: "'")
+        // Add more HTML entities as needed
+        return decodedString
+    }
 }
